7516e45da289145feb1751944b32306086552b04
Mathias Willadsen Brejnebøl <mathiaswbrejne@outlook.com>
2024-12-30 11:13:55 +0100

    more printing

--- lib/sqlite_crdt.dart
+++ lib/sqlite_crdt.dart
@@ -84,6 +84,7 @@ class SqliteCrdt extends SqlCrdt {
     );
 
     final crdt = SqliteCrdt._(db, excludedTables ?? []);
+    print('crdt._excludedTables: ${crdt._excludedTables}');
     await crdt.init();
     return crdt;
   }
@@ -92,6 +93,7 @@ class SqliteCrdt extends SqlCrdt {
 
   @override
   Future<Iterable<String>> getTables() async {
+    print('SqliteCrdt.getTables, _excludedTables: $_excludedTables');
     final tableNames = (await _db.rawQuery('''
         SELECT name FROM sqlite_schema
         WHERE type ='table' AND name NOT LIKE 'sqlite_%' AND name NOT IN (?1)
